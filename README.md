0ca4fb844cf3a7722f6037887b71798c2e1611bf
Nianjun Sun <sunnianjun@sphere-ex.com>
2022-03-26 18:55:28 +0800

    clean the original repo and make this to study python3

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Test
+# Learn Python3 the Hard Way
+
+this is the repo which record all I study python3 by the book 「learn python3 the hard way」
---
+++ exercise-1/ex1.py
@@ -0,0 +1,10 @@
+from cgi import print_arguments
+
+
+print("Hello World!")
+print("Hello Againt")
+print("I like typing this.")
+print("This is fun.")
+print("Yep, printing")
+print("I'd much rather you 'not'")
+print('I "said" not touch this')
